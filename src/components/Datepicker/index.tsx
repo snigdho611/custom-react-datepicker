f45ac07740e2faa0c6083525b63fa55107e04530
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2023-06-02 11:48:54 +0600

    Set up boilerplate for react

---
+++ src/components/Datepicker/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./index.scss"
+
+const DatepickerHeader = () => {
+    return <div>123</div>
+}
+
+const Datepicker = () => {
+  return (
+    <div className='datepicker'></div>
+  )
+}
+
+export default Datepicker
